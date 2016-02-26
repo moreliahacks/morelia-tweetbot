6b9c45879dec67bc414f7ea1674425b9c8393b30
Sebastián Osorio <im@svzosorio.co>
2016-02-26 02:14:52 -0600

    -BugFix remove comments and logs

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ function uploadTweet(tweet) {
     })
     .then(
         function(response){
-            console.log(typeof response);
             return rp({
                 url: mtURL,
                 method: "POST",
@@ -63,67 +62,4 @@ function uploadTweet(tweet) {
             console.log('Upload error');
         }
     );
-
 }
-
-
-/*
-
-action();
-
-function action() {
-
-client.get('search/tweets', {q: 'Morelia'}, function(error, tweets, response){
-tweets.statuses.forEach(function(tweet){
-
-console.log(tweet.text);
-
-rp({
-url: "https://sentimental-language.herokuapp.com/translate?text=" + tweet.text,
-method: 'GET',
-headers: {
-'User-Agent': 'Morelia-Tweet-Bot'
-},
-}).then(
-function(sentimental){
-console.log(sentimental.data);
-rp({
-url: "http://localhost:3001/tweets",
-method: "POST",
-headers: {
-'User-Agent': 'Morelia-Tweet-Bot'
-},
-json: true,
-body: {
-text: tweet.text,
-twitterId: tweet.id,
-categories: ['Morelia'],
-sentiment: sentimental.data,
-date: tweet.created_at,
-location: {
-geo: tweet.geo,
-coordinates: tweet.coordinates ,
-place: tweet.place
-},
-hashtags: tweet.text.match(/(^|\W)(#[a-z\d][\w-]*)/ig),
-mentions: tweet.text.match(/(^|\W)(@[a-z\d][\w-]*)/ig)
-}
-})
-.then(function(success){
-console.log('Success');
-})
-.catch(function(error){
-console.log('Error');
-})
-}
-)
-
-
-});
-});
-
-console.log('Action number: ' + ++conta);
-
-setTimeout(action, 300000);
-}
-*/
