4f58064a73d1a7dc8efad1e791916d4f9b5d1481
Sebastián Osorio <im@svzosorio.co>
2016-02-26 01:56:29 -0600

    Get tweets by stream

--- index.js
+++ index.js
@@ -2,69 +2,130 @@
 
 var Twitter         = require('twitter')
 ,   rp              = require('request-promise')
-,   conta           = 0;
 
 var client = new Twitter({
-    consumer_key: 'vb5Rd7e4qBn1O1aNEeCM5XDBd',
-    consumer_secret: 'ggOnDAdRH94J4WnPJtkMJZ6O9XZTBc5H1xjERUnW7Vx8wteoIv',
-    access_token_key: '926049230-ihXw9odyvwZ2Z7M3mYAquSJp8bjAHSF4vaiZeLdR',
-    access_token_secret: 'epEx5grknutXov9E7pkzK37WwLKO6YOQqFTpZ6B8jcg94'
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
+var mtURL = process.env.PRODUCTION
+? 'http://morelia-tweets.herokuapp.com/tweets' : 'http://localhost:3001/tweets';
+
+client.stream('statuses/filter', {track: 'morelia'},  function(stream){
+    stream.on('data', function(tweet) {
+        uploadTweet(tweet);
+    });
+
+    stream.on('error', function(error) {
+        console.log(error);
+    });
+});
+
+function uploadTweet(tweet) {
+
+    console.log(tweet);
+
+    rp({
+        url: "https://sentimental-language.herokuapp.com/translate?text=" + tweet.text,
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Morelia-Tweet-Bot'
+        },
+    })
+    .then(
+        function(response){
+            console.log(response.data);
+            return rp({
+                url: mtURL,
+                method: "POST",
+                headers: {
+                    'User-Agent': 'Morelia-Tweet-Bot'
+                },
+                json: true,
+                body: {
+                    text: tweet.text,
+                    twitterId: tweet.id,
+                    sentiment: response.data,
+                    date: tweet.created_at,
+                    location: {
+                        geo: tweet.geo,
+                        coordinates: tweet.coordinates ,
+                        place: tweet.place
+                    },
+                    entities: tweet.entities
+                }
+            })
+        }
+    )
+    .then(function(){
+        console.log('Success Upload');
+    })
+    .catch(function(error){
+        console.log('Error');
+    });
+
+}
+
+
+/*
 
 action();
 
 function action() {
 
-    client.get('search/tweets', {q: 'Morelia'}, function(error, tweets, response){
-        tweets.statuses.forEach(function(tweet){
+client.get('search/tweets', {q: 'Morelia'}, function(error, tweets, response){
+tweets.statuses.forEach(function(tweet){
 
-            console.log(tweet.text);
+console.log(tweet.text);
 
-            rp({
-                url: "https://sentimental-language.herokuapp.com/translate?text=" + tweet.text,
-                method: 'GET',
-                headers: {
-                    'User-Agent': 'Morelia-Tweet-Bot'
-                },
-            }).then(
-                function(sentimental){
-                    console.log(sentimental);
-                    rp({
-                        url: "http://localhost:3001/tweets",
-                        method: "POST",
-                        headers: {
-                            'User-Agent': 'Morelia-Tweet-Bot'
-                        },
-                        json: true,
-                        body: {
-                            text: tweet.text,
-                            twitterId: tweet.id,
-                            categories: ['Morelia'],
-                            sentiment: sentimental,
-                            date: tweet.created_at,
-                            location: {
-                                geo: tweet.geo,
-                                coordinates: tweet.coordinates ,
-                                place: tweet.place
-                            },
-                            hashtags: tweet.text.match(/(^|\W)(#[a-z\d][\w-]*)/ig),
-                            mentions: tweet.text.match(/(^|\W)(@[a-z\d][\w-]*)/ig)
-                        }
-                    })
-                    .then(function(success){
-                        console.log('Success');
-                    })
-                    .catch(function(error){
-                        console.log('Error');
-                    })
-                }
-            )
-
-
-        });
-    });
-
-    console.log('Action number: ' + ++conta);
-
-    setTimeout(action, 300000);
+rp({
+url: "https://sentimental-language.herokuapp.com/translate?text=" + tweet.text,
+method: 'GET',
+headers: {
+'User-Agent': 'Morelia-Tweet-Bot'
+},
+}).then(
+function(sentimental){
+console.log(sentimental.data);
+rp({
+url: "http://localhost:3001/tweets",
+method: "POST",
+headers: {
+'User-Agent': 'Morelia-Tweet-Bot'
+},
+json: true,
+body: {
+text: tweet.text,
+twitterId: tweet.id,
+categories: ['Morelia'],
+sentiment: sentimental.data,
+date: tweet.created_at,
+location: {
+geo: tweet.geo,
+coordinates: tweet.coordinates ,
+place: tweet.place
+},
+hashtags: tweet.text.match(/(^|\W)(#[a-z\d][\w-]*)/ig),
+mentions: tweet.text.match(/(^|\W)(@[a-z\d][\w-]*)/ig)
 }
+})
+.then(function(success){
+console.log('Success');
+})
+.catch(function(error){
+console.log('Error');
+})
+}
+)
+
+
+});
+});
+
+console.log('Action number: ' + ++conta);
+
+setTimeout(action, 300000);
+}
+*/
