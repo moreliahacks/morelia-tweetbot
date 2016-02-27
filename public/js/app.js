7ec25bbe2f3e563505baa98e5b904d8d8a5aa9d1
Sebastián Osorio <im@svzosorio.co>
2016-02-27 00:02:11 -0600

    -Feature add basic consumer

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+
+angular
+.module('moreliaConsumer', [])
+.config(config)
+.run(run)
+.controller('MainController', MainController)
+.service('Tweet', Tweet);
+
+function config() {}
+
+function run($rootScope, $http) {
+    console.log('Its running');
+}
+
+function MainController($scope, Tweet, $timeout){
+
+    action();
+
+    function action() {
+        Tweet.list().then(function(tweets){
+            $scope.tweets = tweets.reverse();
+        });
+
+        $timeout(action, 10000);
+    }
+}
+
+function Tweet($http) {
+    this.list = function(){
+        return $http({
+            method: 'GET',
+            url: 'https://morelia-tweets.herokuapp.com/tweets'
+        })
+        .then(function(response){
+            return response.data.data;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+}
